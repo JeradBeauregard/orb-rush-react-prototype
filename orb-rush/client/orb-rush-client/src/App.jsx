b0596c2edfb70abae33b0f9b47510bb3e6b2773a
Jerad Beauregard <jeradbeauregard@gmail.com>
2026-06-19 22:53:34 -0400

    prototyped gameboard

---
+++ orb-rush/client/orb-rush-client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import GameBoard from './components/GameBoard'
+
+function App() {
+
+  return (
+    <>
+
+      <GameBoard/>
+
+      </>
+  )
+}
+
+export default App
